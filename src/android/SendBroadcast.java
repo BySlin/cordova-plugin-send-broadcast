e46aed54193dd49c4841addc022f9d41dbcb9680
BySlin <2292735470@qq.com>
2020-11-06 15:01:49 +0800

    fix

--- src/android/SendBroadcast.java
+++ src/android/SendBroadcast.java
@@ -1,23 +1,17 @@
 package com.byslin.cordova.plugin;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.util.Log;
 
-import com.alibaba.fastjson.JSON;
-import com.android.hdhe.uhf.reader.UhfReader;
-import com.android.hdhe.uhf.readerInterface.TagModel;
-
 import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CordovaWebView;
-import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import cn.pda.serialport.Tools;
 
 public class SendBroadcast extends CordovaPlugin {
 
@@ -26,13 +20,13 @@ public class SendBroadcast extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
         switch (action) {
-            case "start":
-            String action = args.getString(0);
+            case "sendAction":
+                String actionName = args.getString(0);
                 cordova.getThreadPool().execute(new Runnable() {
                     @Override
                     public void run() {
                         final Intent intent = new Intent();
-                        intent.setAction(action);
+                        intent.setAction(actionName);
                         sendExplicitBroadcast(intent);
                     }
                 });
@@ -43,10 +37,9 @@ public class SendBroadcast extends CordovaPlugin {
         }
     }
 
-    private boolean sendExplicitBroadcast(Intent intent) {
-        PackageManager pm = mContext.getPackageManager();
+    private void sendExplicitBroadcast(Intent intent) {
+        PackageManager pm = cordova.getContext().getPackageManager();
         List<ResolveInfo> matches = pm.queryBroadcastReceivers(intent, 0);
-        boolean sent = false;
 
         for (ResolveInfo resolveInfo : matches) {
             ComponentName cn =
@@ -55,9 +48,7 @@ public class SendBroadcast extends CordovaPlugin {
 
             intent.setComponent(cn);
             intent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
-            mContext.sendBroadcast(intent);
-            sent = true;
+            cordova.getContext().sendBroadcast(intent);
         }
-        return sent;
     }
 }
--- www/cordova-plugin-send-broadcast.js
+++ www/cordova-plugin-send-broadcast.js
@@ -1,5 +1,5 @@
 var exec = require("cordova/exec");
 
 exports.sendAction = function (action, success, error) {
-  exec(success, error, "cordova-plugin-send-broadcast", "start", [action]);
+  exec(success, error, "cordova-plugin-send-broadcast", "sendAction", [action]);
 };
